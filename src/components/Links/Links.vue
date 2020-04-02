9b529d7603a4c5293dfe7de6af6bb0d92c585c91
Henrique Melanda <hjdesigner23@gmail.com>
2020-04-02 19:58:19 -0300

    FEATURE HEADER AND NAV MOBILE
    - Created the header component in the mobile version;
    - Created the mobile version menu;
    - Created the link component in a reusable way to be used in the desktop version;
    - Created the component for the logo to use in the desktop and mobile version;
    - In the button to open the menu I used the aria-label to help in the semantics of the page since the button has no text;
    - Created the function that opens and closes the mobile menu;
    - Validation done for the header and menu only shows below 1024px;

---
+++ src/components/Links/Links.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li class="element" v-for="rota in rotas" :key="rota.name">
+      <router-link class="link" :to="rota.path ? rota.path : '/'">{{rota.name}}</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Link',
+  props: {
+    rotas: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/utils/variables-style.scss";
+
+ul {
+  margin-top: 30px;
+}
+li {
+  padding: 8px 22px;
+}
+.link {
+  color: $color-white;
+  font-weight: 400;
+  font-size: 1.4em;
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Logo/Logo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <p class="logo">Logotipo</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/utils/variables-style.scss";
+
+.container {
+  align-items: center;
+  background-color: rgba(255, 255, 255, .6);
+  border-radius: 17px;
+  display: flex;
+  height: 42px;
+  justify-content: center;
+  width: 93px;
+}
+.logo {
+  color: $color-green;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 1.4em;
+  text-transform: uppercase;
+}
+</style>
